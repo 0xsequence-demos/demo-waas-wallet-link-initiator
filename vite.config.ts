d942a1ffdbf2690ef40c0394fa40b0e37a2c0f8c
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2024-08-02 12:28:58 -0700

    fixed base path, changed vite port to 4444

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "",
   plugins: [react()],
-  base: "/demo-waas-wallet-link-initiator/",
+  server: {
+    port: 4444,
+  },
 });
